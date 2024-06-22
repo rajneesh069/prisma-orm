f11cb65654cd02bdbf62b1b6031d3f64e3f76395
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-22 19:35:08 +0530

    added updatedAt column in both the tables

--- src/todoQueries/index.ts
+++ src/todoQueries/index.ts
@@ -19,12 +19,37 @@ async function addTodo({ title, description, userId }: Todo) {
   console.log(res);
 }
 
-// addTodo({ title: "Go to gym", description: "Go to gym at 5", userId: 1 });
+// addTodo({ title: "Go buy Milk", description: "Go buy Milk at 5", userId: 1 });
+// addTodo({
+//   title: "Complete Assignment",
+//   description: "Finish the math assignment by tonight",
+//   userId: 1,
+// });
+// addTodo({
+//   title: "Call Mom",
+//   description: "Call mom to catch up in the evening",
+//   userId: 1,
+// });
 
-async function getTodo(todoId:number) {
-    const res = todo.findFirst({
-        where : {
-            id : todoId
-        }
-    })
+async function getTodo(todoId: number) {
+  const res = await todo.findFirst({
+    where: {
+      id: todoId,
+    },
+  });
+  console.log(res);
 }
+
+// getTodo(1);
+
+async function getTodosForAParticularUser(userId: number) {
+  const res = await todo.findMany({
+    where: {
+      userId,
+    },
+  });
+  console.log(res);
+}
+
+// getTodosForAParticularUser(1);
+
