c9c85f9729ce36e909ce84fcf03c0fb0aa5a65e1
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-22 18:05:46 +0530

    add todo query

--- src/todoQueries/index.ts
+++ src/todoQueries/index.ts
@@ -1,3 +1,22 @@
 import { PrismaClient } from "@prisma/client";
-const { todos } = new PrismaClient();
+const { todo } = new PrismaClient();
 
+type Todo = {
+  title: string;
+  description: string;
+  userId: number;
+};
+
+async function addTodo({ title, description, userId }: Todo) {
+  const res = await todo.create({
+    data: {
+      title,
+      description,
+      userId,
+    },
+  });
+
+  console.log(res);
+}
+
+addTodo({ title: "Go to gym", description: "Go to gym at 5", userId: 1 });
