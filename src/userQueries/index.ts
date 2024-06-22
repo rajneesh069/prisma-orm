64a620816f1491a925d3e69fc7b476ecca3a0226
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-22 17:46:47 +0530

    modify scripts in package.json and segregate user and todo queries

--- src/userQueries/index.ts
+++ src/userQueries/index.ts
@@ -1,13 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-const { user, todo } = new PrismaClient();
+const { user} = new PrismaClient();
 
 async function createUser(
   email: string,
   password: string,
   firstName: string,
   lastName: string,
-  phoneNumber  : string,
-  todo
+  phoneNumber: string
 ) {
   const res = await user.create({
     data: {
@@ -15,7 +14,7 @@ async function createUser(
       firstName,
       lastName,
       password,
-      phoneNumber,todos
+      phoneNumber,
     },
     select: {
       id: true,
@@ -45,3 +44,36 @@ async function updateUser(
   console.log(res);
 }
 updateUser("rajneesh1@gmail.com", { firstName: "Sachin", lastName: "Mishra" });
+
+async function getUsers(email: string) {
+  const res = await user.findFirst({
+    where: {
+      email,
+    },
+    select: {
+      email: true,
+      firstName: true,
+      lastName: true,
+      id: true,
+    },
+  });
+  console.log(res);
+}
+
+getUsers("rajneesh@gmail.com");
+
+async function getUsersWithTodos(email: string) {
+  const res = await user.findFirst({
+    where: {
+      email,
+    },
+    select: {
+      email: true,
+      firstName: true,
+      lastName: true,
+      id: true,
+      todos: true,
+    },
+  });
+}
+getUsersWithTodos("rajneesh@gmail.com");
