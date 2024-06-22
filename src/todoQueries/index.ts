1f62e093acc559d7f8adc52949272fe9bcd11bf4
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-22 18:10:30 +0530

    Add createdAt columns in both todo and user table

--- src/todoQueries/index.ts
+++ src/todoQueries/index.ts
@@ -19,4 +19,12 @@ async function addTodo({ title, description, userId }: Todo) {
   console.log(res);
 }
 
-addTodo({ title: "Go to gym", description: "Go to gym at 5", userId: 1 });
+// addTodo({ title: "Go to gym", description: "Go to gym at 5", userId: 1 });
+
+async function getTodo(todoId:number) {
+    const res = todo.findFirst({
+        where : {
+            id : todoId
+        }
+    })
+}
